2a7d7f4ba0670f4ebb00cdb1892801525585c223
Fatai Kayode Afolabi <nifty_afk@yahoo.com>
2025-08-06 11:06:04 +0100

    Update upload_docker.sh
    stella training

--- upload_docker.sh
+++ upload_docker.sh
@@ -2,7 +2,7 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
-
+# Training Stella
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
